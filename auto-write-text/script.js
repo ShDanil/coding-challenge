92778238788ba04d8e3182c902cfbeefa79404c1
Danila <deniil2607@gmail.com>
2020-09-24 21:54:48 +0300

    3rd  project auto text write

---
+++ auto-write-text/script.js
@@ -0,0 +1,14 @@
+const text = "New text is comming";
+let index = 0;
+
+function createText() {
+  document.body.innerText = text.slice(0, index);
+  index += 1;
+  if (index > text.length) {
+    index = 0;
+  }
+}
+
+setInterval(() => {
+  createText();
+}, 100);
